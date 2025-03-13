32696ba682d3591b0323425e539441ef63162c52
Elias Tamer <eliastamer3@gmail.com>
2025-03-13 18:46:05 +0200

    pushed a simple web server built with express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    const dummyData = {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        message: 'Hello, this is some dummy data!'
+    };
+
+    res.json(dummyData);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
